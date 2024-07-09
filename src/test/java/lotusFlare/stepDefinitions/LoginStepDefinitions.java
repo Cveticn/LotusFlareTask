9fc42e367a05a8cb9df28d687606fd5e663deec9
NenadCvetic <cveticnenad92@gmail.com>
2024-07-09 18:21:22 +0200

    Additional Login page test cases

--- src/test/java/lotusFlare/stepDefinitions/LoginStepDefinitions.java
+++ src/test/java/lotusFlare/stepDefinitions/LoginStepDefinitions.java
@@ -1,5 +1,6 @@
 package lotusFlare.stepDefinitions;
 
+import com.github.javafaker.Faker;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 import lotusFlare.pages.BasePage;
@@ -7,12 +8,16 @@ import lotusFlare.pages.LoginPage;
 import lotusFlare.utilities.ConfigPropertiesReader;
 import lotusFlare.utilities.Driver;
 import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class LoginStepDefinitions {
 
     LoginPage loginPage = new LoginPage();
 
     BasePage basePage = new BasePage();
+
+    Faker faker = new Faker();
 
     @When("user is on the login page")
     public void user_is_on_the_login_page() {
@@ -67,8 +72,56 @@ public class LoginStepDefinitions {
         //asserting error message
         Assert.assertEquals("Epic sadface: Sorry, this user has been locked out.", loginPage.errorMessage.getText());
 
+    }
+
+    @Then("verify that username required error message is displayed")
+    public void verify_that_username_required_error_message_is_displayed() {
+
+        Assert.assertEquals("Epic sadface: Username is required", loginPage.errorMessage.getText());
 
     }
 
+    @Then("verify that password required error message is displayed")
+    public void verify_that_password_required_error_message_is_displayed() {
+
+        Assert.assertEquals("Epic sadface: Password is required", loginPage.errorMessage.getText());
+
+    }
+
+    @When("user enters invalid username")
+    public void user_enters_invalid_username() {
+
+        String invalidUsername = ConfigPropertiesReader.getProperty("username") + faker.letterify("?");
+
+        loginPage.userNameInputBox.sendKeys(invalidUsername);
+
+    }
+    @When("user enters invalid password")
+    public void user_enters_invalid_password() {
+
+        String invalidPassword = ConfigPropertiesReader.getProperty("password") + faker.letterify("?");
+
+
+        loginPage.passwordInputBox.sendKeys(invalidPassword);
+    }
+    @Then("verify that username and password do not match error message is displayed")
+    public void verify_that_username_and_password_do_not_match_error_message_is_displayed() {
+
+        Assert.assertEquals("Epic sadface: Username and password do not match any user in this service", loginPage.errorMessage.getText());
+
+    }
+
+    @When("user clicks tab on keyboard")
+    public void user_clicks_tab_on_keyboard() {
+
+        loginPage.userNameInputBox.sendKeys(Keys.TAB);
+
+    }
+    @When("user clicks enter on keyboard")
+    public void user_clicks_enter_on_keyboard() {
+
+        loginPage.loginButton.sendKeys(Keys.ENTER);
+
+    }
 
 }
--- src/test/resources/features/LoginFunctionality.feature
+++ src/test/resources/features/LoginFunctionality.feature
@@ -10,11 +10,42 @@ Feature: Login functionality of the application
     And user enters valid password
     And user clicks on login button
     Then verify that user is successfully logged into the application
-  @wip
-  Scenario: locked out user can not log in and appropriate message is displayed
+
+  Scenario: locked out user can not log in and appropriate error message is displayed
     When locked out user enters username
     And locked out user enters password
     And user clicks on login button
     Then verify that error message is displayed correctly
+
+
+  Scenario: user can not login without username and appropriate error message is displayed
+    When user enters valid password
+    And user clicks on login button
+    Then verify that username required error message is displayed
+
+
+  Scenario: user can not login without password and appropriate error message is displayed
+    When user enters valid username
+    And user clicks on login button
+    Then verify that password required error message is displayed
+
+
+    Scenario: user can not login with invalid credentials
+      When user enters invalid username
+      And user enters invalid password
+      And user clicks on login button
+      Then verify that username and password do not match error message is displayed
+
+      @wip
+      Scenario: user can use keyboard keys to navigate through login page
+        When user enters valid username
+        And user clicks tab on keyboard
+        And user enters valid password
+        And user clicks enter on keyboard
+        Then verify that user is successfully logged into the application
+
+
+
+
 
 
